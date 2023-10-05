0d2b226a9a2a2f07ef21fdd0c55599025b1f5f71
Aleksey Kalugin <mralexjoin@gmail.com>
2023-10-05 17:18:15 +0300

    Add toasts to buttons

--- app/src/main/java/com/akalugin/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/akalugin/playlistmaker/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.akalugin.playlistmaker
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.settings_button).setOnClickListener(
+            object : View.OnClickListener{
+                override fun onClick(v: View?) = showToastForButton(v!!)
+            }
+        )
     }
+
+    private fun showToastForButton(view: View) =
+        Toast.makeText(
+            this,
+            "Нажата кнопка ${(view as Button).text}",
+            Toast.LENGTH_SHORT
+        ).show()
 }

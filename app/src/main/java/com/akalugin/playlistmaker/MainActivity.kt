794ee5d70f1dabd2a795e5ee7a9a065ef3e479f6
Aleksey Kalugin <mralexjoin@gmail.com>
2023-10-07 12:30:39 +0300

    Remove companion object

--- app/src/main/java/com/akalugin/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/akalugin/playlistmaker/MainActivity.kt
@@ -10,18 +10,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        NAVIGATION_MAP.forEach { (buttonId, activityClass) ->
+        mapOf(
+            R.id.search_button to SearchActivity::class,
+            R.id.library_button to LibraryActivity::class,
+            R.id.settings_button to SettingsActivity::class,
+        ).forEach { (buttonId, activityClass) ->
             findViewById<Button>(buttonId).setOnClickListener {
                 startActivity(Intent(this@MainActivity, activityClass.java))
             }
         }
-    }
-
-    companion object {
-        val NAVIGATION_MAP = mapOf(
-            R.id.search_button to SearchActivity::class,
-            R.id.library_button to LibraryActivity::class,
-            R.id.settings_button to SettingsActivity::class,
-        )
     }
 }
